eb3000b80feffa7a904c135254b785321099b30d
omkargundlur <omkargundlur@gmail.com>
2020-08-28 22:29:25 +0530

    Adding varibles testing

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,16 @@ variable "number" {
   default = 10
   description = "This is testing variables"
 }
+
+variable "list" {
+  default = ["car",10, false, 11.11]
+
+}
+
+output "accesing_variable" {
+    value = var.number
+}
+
+output "list" {
+  value = var.list
+}
